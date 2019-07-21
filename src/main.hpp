10b2ba92673c06f41368ec0f8a94d37aebd54a96
Fred <fredrik.sandq@gmail.com>
2019-07-22 00:40:37 +0200

    some more items

--- src/main.hpp
+++ src/main.hpp
@@ -10,10 +10,12 @@ enum ItemsAndEvents : uint16_t
     //items
     hp1 = 0x0149,
     hp2 = 0x0249,
+    hp7 = 0x0749,
 
     vellum1 = 0x002D,
     potion1 = 0x0A2D,
 
+    buster     = 0x0048,
     tornado    = 0x0248,
     earthCrest = 0x0848,
     airCrest   = 0x0A48,
@@ -33,7 +35,9 @@ namespace Location
         hippogriff1   = 0x011999,
         flameLord     = 0x014CFD,
         somulo        = 0x0196D3,
+        ovnunu        = 0x01C6D4,
         stage1_Potion = 0x02511E,
+        stage1_Hp     = 0x0251A3,
     };
 }
 
@@ -74,22 +78,25 @@ std::vector<uint16_t> itemList
 {
     hp1, hp2,
     vellum1, potion1,
-    tornado, earthCrest, airCrest,
+    buster, tornado, earthCrest, airCrest,
 };
 
 const std::vector<ItemOffsets> itemOffsets
 {
-    {Location::stage1_Vellum, 0}, //Stage 1 vellum
-    {Location::arma1, 1}, //Arma 1
-    {Location::arma2, 1}, //Arma 2
-    {Location::hippogriff1, 1}, //Hippogriff 1
-    {Location::flameLord, 1}, //Flame Lord (| 0x4000)
-    {Location::somulo, 1}, //Somulo
-    {Location::stage1_Potion, 0}, //Stage 1 potion (| 0x4000)
+    {Location::stage1_Vellum, 0},
+    {Location::arma1, 1},
+    {Location::arma2, 1},
+    {Location::hippogriff1, 1},
+    {Location::flameLord, 1}, // | 0x4000
+    {Location::somulo, 1},
+    {Location::ovnunu, 1}, // | 0x4000
+    {Location::stage1_Potion, 0}, // | 0x4000
+    {Location::stage1_Hp, 0}, // 0x4000
 };
 
 const std::map<uint16_t, CompletionEvent> completionEvent
 {
+    {buster    , {0, 0b0000'0001}},
     {tornado   , {0, 0b0000'0010}},
     {earthCrest, {0, 0b0001'0000}},
     {airCrest  , {0, 0b0010'0000}},
@@ -97,8 +104,9 @@ const std::map<uint16_t, CompletionEvent> completionEvent
     {vellum1, {1, 0b0000'0010}},
     {potion1, {1, 0b0100'0000}},
 
-    {hp1, {3, 0b01}},
-    {hp2, {3, 0b10}},
+    {hp1, {3, 0b0000'0001}},
+    {hp2, {3, 0b0000'0010}},
+    {hp7, {3, 0b0100'0000}},
 
     {stage1_Statue, {5, 0b01}},
     {stage1_Potion, {5, 0b10}},
@@ -120,10 +128,12 @@ const std::map<uint16_t, std::string> itemNames
 {
     {hp1, "Hp 1"},
     {hp2, "Hp 2"},
+    {hp7, "Hp 7"},
 
     {vellum1, "Vellum 1"},
     {potion1, "Potion 1"},
 
+    {buster,     "Buster"},
     {tornado,    "Tornado"},
     {earthCrest, "Earth crest"},
     {airCrest,   "Air crest"},
@@ -137,5 +147,7 @@ const std::map<uint32_t, std::string> locationNames
     {Location::hippogriff1,   "Hippogriff 1"},
     {Location::flameLord,     "Flame Lord"},
     {Location::somulo,        "Somulo"},
+    {Location::ovnunu,        "Ovnunu"},
     {Location::stage1_Potion, "Stage 1 potion"},
+    {Location::stage1_Hp,     "Stage 1 Hp"},
 };
